0103c041c14a40b254b0de1959f9b5962ec3547e
Junlin Li <jlli0410@uw.edu>
2017-12-11 21:03:38 -0800

    cleaned up and made test more detailed and well-organized, completed

--- objectdetection/tests/test.py
+++ objectdetection/tests/test.py
@@ -6,23 +6,75 @@ import numpy as np
 sys.path.insert(0, '../')
 from submodule import appfunction as appfun
 
+'''
+This class tests main functions of this detector.
+''' 
 class Object_detector_test(unittest.TestCase):
-    # '''
-    # This class tests main function used in this detector.
-    # ''' 
+    '''
+    Test the function of detecting static image
+    '''
     def test_input_image(self):
         img_input = "test_image.jpg"
-        modelPath = '../submodule/ssd_mobilenet_v1_coco_11_06_2017/frozen_inference_graph.pb'
+        modelPath = '../ssd_mobilenet_v1_coco_11_06_2017' + \
+                    '/frozen_inference_graph.pb'
         labelMapPath = '../data/mscoco_label_map.pbtxt'
-        testval, image_done = appfun.input_image(img_input, modelPath, labelMapPath, None)
+        image_done, testval = appfun.input_image(img_input,
+                                                 modelPath,
+                                                 labelMapPath,
+                                                 "",
+                                                 False
+                                                 )
         self.assertTrue(np.isclose(testval, -1))
     
-    def test_input_cam(self):
-        modelPath = '../ssd_mobilenet_v1_coco_11_06_2017/frozen_inference_graph.pb'
+    '''
+    Test the function of detecting video
+    '''
+    def test_input_video(self):
+        modelPath = '../ssd_mobilenet_v1_coco_11_06_2017/' + \
+                    'frozen_inference_graph.pb'
         labelMapPath = '../data/mscoco_label_map.pbtxt'
-        self.assertTrue(np.isclose(appfun.input_cam(modelPath, labelMapPath, None, True), 1))
+        video_file = 'test_video.mp4'
+        self.assertTrue(
+            np.isclose(appfun.input_video(video_file,
+                                          modelPath,
+                                          labelMapPath,
+                                          "",
+                                          True
+                                          ), 1)
+            )
+
+    '''
+    Test the function of detecting live camera
+    '''
+    def test_input_cam(self):
+        modelPath = '../submodule/ssd_mobilenet_v1_coco_11_06_2017/' + \
+                    'frozen_inference_graph.pb'
+        labelMapPath = '../data/mscoco_label_map.pbtxt'
+        self.assertTrue(
+            np.isclose(appfun.input_cam(modelPath,
+                                        labelMapPath,
+                                        "",
+                                        True
+                                        ), 1)
+            )
+
+    '''
+    Test the helper function of detecting video/live camera
+    '''
+    def test_video_open(self):
+        modelPath = '../submodule/ssd_mobilenet_v1_coco_11_06_2017/' + \
+                    'frozen_inference_graph.pb'
+        labelMapPath = '../data/mscoco_label_map.pbtxt'
+        cap = cv2.VideoCapture("")
+        with self.assertRaises(Exception):
+            appfun.video_open(cap,
+                              False,
+                              modelPath,
+                              labelMapPath,
+                              "",
+                              True)
+        self.assertTrue(True)
 
 # if __name__ == '__main__':
-#     unittest.main()
 suite = unittest.TestLoader().loadTestsFromTestCase(Object_detector_test)
 unittest.TextTestRunner(verbosity=2).run(suite)
