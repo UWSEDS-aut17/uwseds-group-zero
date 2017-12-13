32f6baece3b95ef01feec86d72999f003896f041
Junlin Li <jlli0410@uw.edu>
2017-12-12 22:49:36 -0800

    slight style changed in setup.py

--- setup.py
+++ setup.py
@@ -9,36 +9,23 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='uwseds-group-zero',
-
     version='1.0',
-
     description='an object detection project for UW CSE583 17Au',
     long_description=long_description,
-
     url='https://github.com/UWSEDS-aut17/uwseds-group-zero',
-
     author='Li Junlin, Li Tianqi, Nan Qiao, Dizhi Ma, Xiaohan Wang',
     author_email='{jlli0410, tqli3, nqiao, dizhim, xhwang}@uw.edu',
-
     license='MIT',
-
     classifiers=[
         'Development Status :: 3 - Alpha',
-
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-
         'License :: OSI Approved :: MIT License',
-
         'Programming Language :: Python :: 3.5',
     ],
-
     keywords='object detection',
-
     packages=find_packages(),
-
-    install_requires=['tensorflow', 'tkinter', 'Pillow', 'opencv-python'],
-
+    install_requires=['tensorflow==1.4.0', 'tkinter', 'Pillo>=1.0', 'opencv-python', 'pycodestyle'],
     python_requires='>=3.5, !=3.6.*, <4',
 
 )
