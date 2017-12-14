5eefbb20d3eab5b1443e919507a36704ef206e24
Junlin Li <jlli0410@uw.edu>
2017-12-13 22:34:15 -0800

    edited test and setup and appfunction to meet pep8 and more unit test coverage

--- objectdetection/function/appfunction.py
+++ objectdetection/function/appfunction.py
@@ -1,11 +1,5 @@
 import sys
 import os
-# sys.path.insert(
-#     0,
-#     os.path.abspath(
-#         os.path.join(os.path.dirname(__file__), "/..")
-#         )
-#     )
 from . import object_detection_main as od
 import tensorflow as tf
 from PIL import Image
